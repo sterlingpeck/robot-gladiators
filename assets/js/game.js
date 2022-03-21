a80fb4d2d1bb5c44d44a717ed7656065e70c6461
Sterling Peck <peck.sterling@gmail.com>
2022-03-21 16:38:03 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,42 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
+var playerAttack = 10;
 var playerMoney = 10;
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var playerHealth = 50;
+var enemyAttack = 12;
+
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    enemyHealth = enemyHealth - playerAttack;
+  
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    )
+  
+    //Check enemies health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " had died!");
+    } 
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+  
 
 //check to see if the value of the playerHealth variable is greater than 0
 if (playerHealth > 0) {
@@ -19,28 +55,15 @@ else {
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-  window.alert("Welcome to Robot Gladiators!");
-
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-  enemyHealth = enemyHealth - playerAttack;
-
-  // Log a resulting message to the console so we know that it worked.
-  console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-  )
-
-  //Check enemies health
-  if (enemyHealth <= 0) {
-      window.alert(enemyName + " had died!");
-  } 
-  else {
-      window.alert(enemyName + " still has " + enemyHealth + " health left.");
-  }
 
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
   playerHealth = playerHealth - enemyAttack;
@@ -101,6 +124,8 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
     }
     // if no (false), ask question again by running fight() again
     else {
-      fight();
+    for(var i = 0; i < enemyNames.length; i++) {
+        fight(enemyNames[i]);
     }
   }
+}
