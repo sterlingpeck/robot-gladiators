988217fc963c28c8a448f04685935284534b4b20
Sterling Peck <peck.sterling@gmail.com>
2022-03-23 09:58:15 -0600

    playername bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -168,10 +168,18 @@ var randomNumber = function(min, max) {
   /* END GAME FUNCTIONS */
   
   /* GAME INFORMATION / VARIABLES */
+  var playerInfo = {
+    name: getPlayerName(),
+  var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+      }
+    console.log("Your robot's name is " + name);
+    return name;
+  };
   
   // player information
-  var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
     health: 100,
     attack: 10,
     money: 10,
