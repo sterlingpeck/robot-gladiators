1f76ac7bb17549b7945561114de817a7dc07b473
Sterling Peck <peck.sterling@gmail.com>
2022-03-23 10:51:06 -0600

    added shop-input feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -168,34 +168,35 @@ var randomNumber = function(min, max) {
   // go to shop between battles function
   var shop = function() {
     // ask player what they'd like to do
-    var shopOptionPrompt = window.prompt(
+    var shopOptionPrompt = parseInt(shopOptionPrompt);(
       'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
     );
   
     // use switch case to carry out action
     switch (shopOptionPrompt) {
-      case 'REFILL':
-      case 'refill':
-        playerInfo.refillHealth();
-        break;
-      case 'UPGRADE':
-      case 'upgrade':
-        playerInfo.upgradeAttack();
-        break;
-      case 'LEAVE':
-      case 'leave':
-        window.alert('Leaving the store.');
+        case 1:
+          playerInfo.refillHealth();
+          break;
+        case 2:
+          playerInfo.upgradeAttack();
+          break;
+        case 3:
+          window.alert("Leaving the store.");
+          break;
+        default:
+          window.alert("You did not pick a valid option. Try again.");
+          shop();
+          break;
+      }
   
         // do nothing, so function will end
         break;
-      default:
+        default
         window.alert('You did not pick a valid option. Try again.');
   
         // call shop() again to force player to pick a valid option
         shop();
         break;
-    }
-  };
   
   /* END GAME FUNCTIONS */
   
